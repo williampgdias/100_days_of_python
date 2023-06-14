b78a141599332409b9f3ba7c3a1ede3b33985519
William Dias <smdwilliam89@gmail.com>
2023-06-14 12:37:38 +0100

    day_08 | 01_function_with_inputs

---
+++ part_01-04/day_08/01_functions_with_inputs.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello.")
+    print("How are you?")
+    print("I am learning Python.")
+
+
+greet()
+
+
+def greet_with_name(name):
+    print(f"Hello, {name}")
+    print(f"How are you, {name}?")
+
+
+greet_with_name("William")
