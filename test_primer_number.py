eff9d2716d6914af8f05bf821d6dbf16ef95fdd2
William Dias <smdwilliam89@gmail.com>
2023-06-14 13:46:51 +0100

    exercise_02

---
+++ test_primer_number.py
@@ -0,0 +1,11 @@
+def prime_checker(number):
+    if number % number:
+        print("It's a primer number")
+    else:
+        print("It's not a primer number")
+# Write your code above this line 👆
+
+
+# Do NOT change any of the code below👇
+# n = int(input("Check this number: "))
+prime_checker(2)
