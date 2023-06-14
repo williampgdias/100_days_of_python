e3ec9bd82870dbc1058752bf4cbea309f96dfe69
William Dias <smdwilliam89@gmail.com>
2023-06-14 16:35:13 +0100

    Caesar Cipher project 03/04 - Reorganising the code

--- part_01-04/day_08/project_caesar_cipher.py
+++ part_01-04/day_08/project_caesar_cipher.py
@@ -7,36 +7,39 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
+# TODO_1: COMBINE THE ENCRYPT() AND DECRYPT() FUNCTIONS INTO A SINGLE FUNCTION CALLED CAESAR().
+############################## MY CODE! ITS WORKING!! ##############################
+# def caesar(plain_text, cipher_text, shift_amount):
+#     if direction == "encode":
+#         cipher_text = ""
+#         for letter in plain_text:
+#             position = alphabet.index(letter)
+#             new_position = position + shift_amount
+#             new_letter = alphabet[new_position]
+#             cipher_text += new_letter
+#         print(f"The encoded text is {cipher_text}")
+#     elif direction == "decode":
+#         plain_text = ""
+#         for letter in cipher_text:
+#             position = alphabet.index(letter)
+#             new_position = position - shift_amount
+#             new_letter = alphabet[new_position]
+#             plain_text += new_letter
+#         print(f"The decoded text is {plain_text}")
+
+############################## TEACHER CODE!! ##############################
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
 
 
-# TODO_1: CREATE A DIFFERENT FUNCTION CALLED "DECRYPT" THAT TAKES THE "TEXT" AND "SHIFT" AS INPUTS.
-def decrypt(cipher_text, shift_amount):
-    # TODO_2: INSIDE THE 'DECRYPT' FUNCTION, SHIFT EACH LETTER OF THE 'TEXT' *BACKWARDS* IN THE ALPHABET BY THE
-    # SHIFT AMOUNT AND PRINT THE DECRYPTED TEXT.
-    # E.G.
-    # cipher_text = "mjqqt"
-    # shift = 5
-    # plain_text = "hello"
-    # print_output: "The decoded text is hello"
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decoded text is {plain_text}")
+# TODO_2: CALL THE CAESAR() FUNCTION, PASSING OVER THE "TEXT", "SHIFT" AND "DIRECTION" VALUES.
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
 
-
-if direction == "encode":
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-    decrypt(cipher_text=text, shift_amount=shift)
 
