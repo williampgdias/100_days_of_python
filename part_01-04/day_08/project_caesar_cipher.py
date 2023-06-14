41097ded986f6c62b8debd29c4e79f47d2e0f675
William Dias <smdwilliam89@gmail.com>
2023-06-14 16:54:35 +0100

    Caesar Cipher project 04/04 - User ExperienceImprovements and Final Touches

--- part_01-04/day_08/project_caesar_cipher.py
+++ part_01-04/day_08/project_caesar_cipher.py
@@ -1,45 +1,52 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-
-# TODO_1: COMBINE THE ENCRYPT() AND DECRYPT() FUNCTIONS INTO A SINGLE FUNCTION CALLED CAESAR().
-############################## MY CODE! ITS WORKING!! ##############################
-# def caesar(plain_text, cipher_text, shift_amount):
-#     if direction == "encode":
-#         cipher_text = ""
-#         for letter in plain_text:
-#             position = alphabet.index(letter)
-#             new_position = position + shift_amount
-#             new_letter = alphabet[new_position]
-#             cipher_text += new_letter
-#         print(f"The encoded text is {cipher_text}")
-#     elif direction == "decode":
-#         plain_text = ""
-#         for letter in cipher_text:
-#             position = alphabet.index(letter)
-#             new_position = position - shift_amount
-#             new_letter = alphabet[new_position]
-#             plain_text += new_letter
-#         print(f"The decoded text is {plain_text}")
-
-############################## TEACHER CODE!! ##############################
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        end_text += alphabet[new_position]
+    # TODO_3: What happens if the user enters a number/symbol/space?
+    # Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    # e.g. start_text = "meet me at 3"
+    # end_text = "•••• •• •• 3"
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+
     print(f"The {cipher_direction}d text is {end_text}")
 
 
-# TODO_2: CALL THE CAESAR() FUNCTION, PASSING OVER THE "TEXT", "SHIFT" AND "DIRECTION" VALUES.
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+# TODO_1: Import and print the logo from art.py when the program starts.
+print(logo)
 
+# TODO_4: Can you figure out a way to ask the user if they want to restart the cipher program?
+# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+should_continue = True
+should_end = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
+    # TODO_2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+    # Try running the program and entering a shift number of 45.
+    # Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+    # Hint: Think about how you can use the modulus (%).
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    result = input("Type 'yes' if tou want to go again. Otherwise type 'no'.\n")
+
+    if result == "no":
+        should_continue = False
+        print("Goodbye.")
