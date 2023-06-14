ada4d2bb0ba49d9d7238b001982f43ea23308d7a
William Dias <smdwilliam89@gmail.com>
2023-06-14 12:54:57 +0100

    04 - Positional vs. Keyword Arguments

---
+++ part_01-04/day_08/02_positional_vs_keyword_arguments.py
@@ -0,0 +1,10 @@
+# FUNCTIONS WITH MORE THAN 1 INPUT
+def greet_with(name, location):
+    print(f"My name is {name}")
+    print(f"I live in {location} with my family.")
+
+
+greet_with("William", "Enniscorthy")
+
+# KEYWORD ARGUMENTS
+greet_with(location="Enniscorthy", name="William")
